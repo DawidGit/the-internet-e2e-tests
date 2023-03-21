fa35393b6f3d6c95ccfd91a2f82b38ce97e3ca58
dawid <dawid.widulinski@gmail.com>
2023-03-21 17:58:46 +0100

    modified ports

--- selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
+++ selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class TheInternetAppMainPage {
 
 
-    public final String mainPageAddress = "http://localhost:5000/";
+    public final String mainPageAddress = "http://localhost:80/";
 
     public final SelenideElement welcomeSign = $("h1.heading");
 
