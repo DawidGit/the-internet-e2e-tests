e9179dd9333712d1c676cd8941e9d86ec5e14ebe
dawid <dawid.widulinski@gmail.com>
2023-03-22 11:01:26 +0100

    github pipeline modified

--- selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
+++ selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class TheInternetAppMainPage {
 
 
-    public final String mainPageAddress = "http://127.0.0.1:5000/";
+    public final String mainPageAddress = "http://localhost:5000/";
 
     public final SelenideElement welcomeSign = $("h1.heading");
 
