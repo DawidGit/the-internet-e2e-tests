633440af116ffcab55d994c43948991a97279095
dawid <dawid.widulinski@gmail.com>
2023-03-23 01:02:50 +0100

    github pipeline modified

--- selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
+++ selenium-tests/src/test/java/com/example/seleniumtests/TheInternetAppMainPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class TheInternetAppMainPage {
 
 
-    public final String mainPageAddress = "http://172.17.0.2:5000/";
+    public final String mainPageAddress = "http://172.17.0.5:5000/";
 
     public final SelenideElement welcomeSign = $("h1.heading");
 
